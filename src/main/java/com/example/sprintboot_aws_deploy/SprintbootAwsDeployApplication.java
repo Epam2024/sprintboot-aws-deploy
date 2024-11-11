aad551d145ec246742e9cef1389e9d9bd463bf70
root <root@EPINPUNW0777>
2024-11-11 11:58:35 +0530

    first commit

---
+++ src/main/java/com/example/sprintboot_aws_deploy/SprintbootAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintboot_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootAwsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintboot_aws_deploy/SprintbootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintboot_aws_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
